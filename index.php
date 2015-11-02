c6c778a651be61cc80c139b24e6e3fa1bbaccdb7
Mubyana <muby3720@yahoo.com>
2015-11-01 21:27:50 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php 
+
+// Script Error Reporting
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+<?php 
+// Run a select query to get my letest 6 items
+// Connect to the MySQL database  
+include "storescripts/connect_to_mysql.php"; 
+$dynamicList = "";
+$sql = mysql_query("SELECT * FROM product ORDER BY date_added DESC LIMIT 6");
+$productCount = mysql_num_rows($sql); // count the output amount
+if ($productCount > 0) {
+	while($row = mysql_fetch_array($sql)){ 
+             $id = $row["id"];
+			 $product_name = $row["product_name"];
+			 $price = $row["price"];
+			 $date_added = strftime("%b %d, %Y", strtotime($row["date_added"]));
+			 $dynamicList .= '<table width="100%" border="0" cellspacing="0" cellpadding="6">
+        <tr>
+          <td width="17%" valign="top"><a href="product.php?"><img style="border:#666 1px solid;" src="inventory_images/' . $id . '.jpg" alt="' . $product_name . '" width="77" height="102" border="1" /></a></td>
+          <td width="83%" valign="top">'.$product_name.'<br />
+          $'.$price.'<br/>
+            <a href="product.php?id=' .$id.'">View Product Details</a></td>
+        </tr>
+      </table>';
+    }
+} else {
+	$dynamicList = "We have no products listed in our store yet";
+}
+mysql_close();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+     <meta charset='utf-8'>
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="style/style.css">
+     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+     <script src="script.js"></script>
+     <title>home page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+<body>
+<div id="mainWrapper">	         
+  <?php include_once("template_header.php");?>
+  <br /><br /><br />
+
+
+<img src="Images/PS4.png" class="imgLeft" /> 
+<h3>Title 1</h3>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lectus urna,
+    viverra in luctus quis, ullamcorper quis lorem. Vestibulum vulputate pellentesque
+    velit, et placerat dolor pulvinar in. Class aptent taciti sociosqu ad litora torquent
+    per conubia nostra, per inceptos himenaeos. Sed sit amet velit at purus elementum
+    dapibus. Nulla dapibus auctor vulputate. Sed cursus nisi at mauris mollis semper.
+    Vestibulum consectetur cursus dui sit amet pretium.
+</p>
+
+<img src="Images/whitexbox1.png"  class="imgRight"/>
+<h3>Title 2</h3>
+<p>
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+    himenaeos. Donec porttitor placerat nunc a consectetur. Ut fermentum eleifend molestie.
+    Donec fermentum risus sit amet ante cursus cursus. In hac habitasse platea dictumst.
+    Praesent semper ante ut felis molestie aliquet. Nam quis dui a magna molestie blandit
+    id et justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+    per inceptos himenaeos. Sed felis mauris, mattis a vulputate a, gravida nec metus.
+    Vivamus elit augue, ullamcorper eget scelerisque vitae, tincidunt eget lorem. Curabitur
+    id nibh libero, sed pulvinar nisi. Curabitur ultrices, neque in dignissim viverra,
+    justo nisl dignissim magna, a tempor eros turpis in diam. Suspendisse potenti. Sed
+    tincidunt est ac elit bibendum nec varius ante vestibulum.
+</p>
+
+<img src= "Images/wiiuz.jpg" class="imgLeft" />
+<h3>Title 3</h3>
+<p>
+    In hac habitasse platea dictumst. In purus leo, consequat nec porta a, varius sed
+    dui. Nulla placerat mollis tincidunt. Praesent at elit mi, a posuere quam. Suspendisse
+    vel leo eu orci lacinia vestibulum. Fusce vel sem fermentum lectus tempus ultrices.
+    Morbi arcu sem, rhoncus sit amet rutrum eget, interdum sit amet leo. Ut mattis lorem
+    vitae dolor tincidunt mattis a lacinia velit. Cras sed nibh at urna imperdiet laoreet
+    quis a risus.
+</p>
+<div>
+  <?php include_once("template_footer.php");?>
+</div>
+</div>
+
+</body>
+</html>
+
