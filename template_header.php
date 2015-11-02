7d68ef7a8a76264e48f8e7632474214516df2bc9
Mubyana <muby3720@yahoo.com>
2015-11-01 21:29:03 -0600

    Create template_header.php

---
+++ template_header.php
@@ -0,0 +1,61 @@
+ <div id="wrapper">
+            <div id="banner">             
+ </div>
+<div id='cssmenu'>
+<ul>
+   <li><a href='index.php'><span>Home</span></a></li>
+   <li class='active has-sub'><a href='#'><span>Consoles</span></a>
+      <ul>
+      	 <li><a href='console_list.php'><span>ALL</span></a>
+         </li>
+         <li class='has-sub'><a href='#'><span>Playstation</span></a>
+            <ul>
+               <li><a href='product_list.php'><span>PS4</span></a></li>
+               <li><a href=''><span>PS3</span></a></li>
+               <li class='last'><a href='#'><span>PS Vita</span></a></li>
+            </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Xbox</span></a>
+            <ul>
+               <li><a href='#'><span>Xbox one</span></a></li>
+               <li class='last'><a href='#'><span>Xbox 360</span></a></li>
+            </ul>
+         </li>
+            <li class='has-sub'><a href='#'><span>Nintendo</span></a>
+            <ul>
+               <li><a href='#'><span>Wii u</span></a></li>
+               <li class='last'><a href='#'><span>Nintendo 3DS</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+    <li class='active has-sub'><a href='#'><span>Games</span></a>
+      <ul>
+      	 <li><a href='game_list.php'><span>ALL</span></a>
+         </li>
+         <li class='has-sub'><a href='#'><span>Playstation</span></a>
+            <ul>
+               <li><a href=''><span>PS4</span></a></li>
+               <li><a href=''><span>PS3</span></a></li>
+               <li class='last'><a href='#'><span>PS Vita</span></a></li>
+            </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Xbox</span></a>
+            <ul>
+               <li><a href='#'><span>Xbox one</span></a></li>
+               <li class='last'><a href='#'><span>Xbox 360</span></a></li>
+            </ul>
+         </li>
+            <li class='has-sub'><a href='#'><span>Nintendo</span></a>
+            <ul>
+               <li><a href='#'><span>Wii u</span></a></li>
+               <li class='last'><a href='#'><span>Nintendo 3DS</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li class='last'><a href='cart.php'><span>Shop cart</span></a></li>
+</ul>
+</div>
+
+
